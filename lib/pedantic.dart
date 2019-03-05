dc42fa885dec6c77b43f0f66f4b712c6b068066e
Rick van Gemert <rickvangem@gmail.com>
2019-03-05 09:51:59 +0100

    Fixed typo in doc string

--- lib/pedantic.dart
+++ lib/pedantic.dart
@@ -6,7 +6,7 @@ import 'dart:async' show Future;
 
 /// Indicates to tools that [future] is intentionally not `await`-ed.
 ///
-/// In an `async` context, it is normally expected than all [Future]s are
+/// In an `async` context, it is normally expected that all [Future]s are
 /// awaited, and that is the basis of the lint `unawaited_futures`. However,
 /// there are times where one or more futures are intentionally not awaited.
 /// This function may be used to ignore a particular future. It silences the
